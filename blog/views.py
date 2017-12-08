81fc249f115ae0c1cbfa217a22e020ee0f9969c4
alabugevaaa <aleksandrachka128@mail.ru>
2017-12-08 11:36:18 +0900

    Added view and template for detailed blog post as well as CSS

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #достаем посты из БД, осторитированные по дате публикации
 	return render(request, 'blog/post_list.html', {'posts': posts}) #собираем шаблон страницы постов, передаем ему список постов
+
+def post_detail(request,pk):
+	post = get_object_or_404(Post, pk=pk) #получаем пос по ключу или выводим 404
+	return render(request, 'blog/post_detail.html', {'post': post}) #собираем ответ и шаблон
